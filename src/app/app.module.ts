64dfbf699f8468a28acab875752012b4d049284f
kaneeuro <mody.kane16@gmail.com>
2018-05-21 09:51:06 +0000

    initial commit
    First version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { PremierComponent } from './premier/premier.component';
+import { DeuxiemeComponent } from './deuxieme/deuxieme.component';
+import { TroisiemeComponent } from './troisieme/troisieme.component';
+import { QuatriemeComponent } from './quatrieme/quatrieme.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'deuxieme', component: DeuxiemeComponent },
+  { path: 'troisieme', component: TroisiemeComponent },
+  { path: 'quatrieme', component: QuatriemeComponent },
+  { path: '', redirectTo: '/premier' , pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    PremierComponent,
+    DeuxiemeComponent,
+    TroisiemeComponent,
+    QuatriemeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/quatrieme/quatrieme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quatrieme',
+  templateUrl: './quatrieme.component.html',
+  styleUrls: ['./quatrieme.component.css']
+})
+export class QuatriemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
